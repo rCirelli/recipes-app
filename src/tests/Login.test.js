2973e22b06bb886d74e3d7b5d40b3f3b70d91f62
Mendri <marcalmendrighi@gmail.com>
2022-07-14 19:47:16 -0300

    Requisito 6

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
-import renderWithRouter from './renderWithRouter';
+import { renderWithRouter } from './renderWithRouter';
 
 describe('Testa a tela de Login', () => {
   it('Testa os campos do formulário', () => {
