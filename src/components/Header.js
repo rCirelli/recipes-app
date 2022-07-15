1719c44879d75c08213f0587696609a3a1d16634
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-07-14 21:15:27 -0300

    requisito 08 e 09 - feito botão de pesquisar e teste do header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,41 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import profileIcon from '../images/profileIcon.svg';
+import searchIcon from '../images/searchIcon.svg';
 
 function Header({ title, withSearchButton }) {
+  const [isSearchBarVisible, setIsSearchBarVisible] = useState(false);
+
+  function toggleSearchBar() {
+    setIsSearchBarVisible(!isSearchBarVisible);
+  }
+
   return (
     <header>
-      <img
-        src="../images/profileIcon.svg"
-        data-testid="profile-top-btn"
-        alt="Profile icon"
-      />
+      <Link to="/profile">
+        <img
+          src={ profileIcon }
+          data-testid="profile-top-btn"
+          alt="Profile icon"
+        />
+      </Link>
       <h1 data-testid="page-title">{title}</h1>
       {
         withSearchButton
-        && <img
-          src="../images/searchIcon.svg"
-          data-testid="search-top-btn"
-          alt="Search Icon"
-        />
+        && (
+          <button type="button" onClick={ toggleSearchBar }>
+            <img
+              src={ searchIcon }
+              data-testid="search-top-btn"
+              alt="Search Icon"
+            />
+          </button>
+        )
+      }
+      {
+        isSearchBarVisible
+        && <input data-testid="search-input" type="text" />
       }
     </header>
   );
