1264ae63345bca1e1b4cb33fc171070f18c1577f
Mendri <marcalmendrighi@gmail.com>
2022-07-15 16:14:49 -0300

    Requisito 60, 61, 62

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Profile() {
   const [userEmail, setUserEmail] = useState('');
+
+  function handleLogout() {
+    localStorage.clear();
+  }
 
   useEffect(() => {
     const storageUserEmail = localStorage.getItem('user');
@@ -15,9 +20,28 @@ function Profile() {
       <Header title="Profile" />
       <div>
         <p data-testid="profile-email">{ userEmail }</p>
-        <button data-testid="profile-done-btn" type="button">Done Recipes</button>
-        <button data-testid="profile-favorite-btn" type="button">Favorite Recipes</button>
-        <button data-testid="profile-logout-btn" type="button">Logout</button>
+        <Link to="/done-recipes">
+          <button data-testid="profile-done-btn" type="button">Done Recipes</button>
+        </Link>
+        <Link to="/favorite-recipes">
+          <button
+            data-testid="profile-favorite-btn"
+            type="button"
+          >
+            Favorite Recipes
+
+          </button>
+        </Link>
+        <Link to="/">
+          <button
+            onClick={ handleLogout }
+            data-testid="profile-logout-btn"
+            type="button"
+          >
+            Logout
+
+          </button>
+        </Link>
       </div>
       <Footer />
     </div>
---
+++ src/tests/Profile.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testa a tela Profile', () => {
+  it('Testa os campos da tela Profile', () => {
+    renderWithRouter(<App />);
+
+    const testEmail = 'test@test.test';
+    const inputEmail = screen.getByText(/insira seu e-mail/i);
+    const inputPassword = screen.getByText(/insira sua senha/i);
+    const loginBtn = screen.getByRole('button', { name: /enter/i });
+
+    userEvent.type(inputEmail, testEmail);
+    userEvent.type(inputPassword, '1234567');
+    userEvent.click(loginBtn);
+
+    const profileIco = screen.getByRole('img', { name: /profile icon/i });
+
+    userEvent.click(profileIco);
+
+    const testEmailElement = screen.getByText(/test@test\.test/i);
+    const doneRecipesBtn = screen.getByRole('button', { name: /done recipes/i });
+    const favoriteRecipesBtn = screen.getByRole('button', { name: /favorite recipes/i });
+    const logoutBtn = screen.getByRole('button', { name: /logout/i });
+
+    expect(testEmailElement).toBeInTheDocument();
+    expect(doneRecipesBtn).toBeInTheDocument();
+    expect(favoriteRecipesBtn).toBeInTheDocument();
+    expect(logoutBtn).toBeInTheDocument();
+
+    userEvent.click(logoutBtn);
+  });
+});
