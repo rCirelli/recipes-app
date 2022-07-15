b8431a0ebca1a3e1ed2a960605354ffe31cdd4ed
Raphael Cirelli <raphael.cirelli@protonmail.com>
2022-07-15 19:23:40 -0300

    corrige os testid e estilização

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -12,68 +12,121 @@ function RecipeDetails({ recipeType, match: { params: { id } } }) {
   useEffect(() => {
     const endpoints = {
       food: 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=',
-      drinks: 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=',
+      drink: 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=',
     };
 
     setEndpoint(`${endpoints[recipeType]}${id}`);
   }, [id, recipeType, setEndpoint]);
 
   useEffect(() => {
-    if (apiResponse.meals) {
+    if (apiResponse.meals || apiResponse.drinks) {
       const details = Object.values(apiResponse)[0][0];
       const ingredients = Object.entries(details)
         .filter(([key]) => key.includes('Ingredient'))
         .map(([, ingredient]) => ingredient);
+      const measures = Object.entries(details)
+        .filter(([key]) => key.includes('Measure'))
+        .map(([, measure]) => measure);
 
-      setRecipeDetails({ ...details, ingredients });
+      setRecipeDetails({ ...details, ingredients, measures });
       setIsFetching(false);
     }
   }, [apiResponse]);
 
+  const type = {
+    food: {
+      thumbnail: 'strMealThumb',
+      name: 'strMeal',
+      category: 'strCategory',
+    },
+    drink: {
+      thumbnail: 'strDrinkThumb',
+      name: 'strDrink',
+      category: 'strAlcoholic',
+    },
+  };
+
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center">
+    <div className="min-h-screen max-w-screen flex flex-col justify-center items-center">
       {
         isFetching ? <Loader /> : (
           <div>
             <img
               data-testid="recipe-photo"
-              src={ recipeDetails.strMealThumb }
-              alt={ recipeDetails.strMeal }
+              src={ recipeDetails[type[recipeType].thumbnail] }
+              alt={ recipeDetails[type[recipeType].name] }
             />
-            <h1
-              data-testid="recipe-title"
-            >
-              {recipeDetails.strMeal}
-            </h1>
-            <h2
-              data-testid="recipe-category"
-            >
-              {recipeDetails.strCategory}
-            </h2>
-            <ul>
-              {
-                recipeDetails?.ingredients.map((ingredient, index) => (
-                  <li
-                    key={ index }
-                    data-testid={ String(index).concat('-ingredient-name-and-measure') }
-                  >
-                    {ingredient}
-                  </li>
-                ))
-              }
-            </ul>
-            <span>
-              {recipeDetails.strInstructions}
-            </span>
+            <div className="px-7 py-2">
+              <h1
+                data-testid="recipe-title"
+                className="text-2xl font-medium"
+              >
+                {recipeDetails[type[recipeType].name]}
+              </h1>
+              <h2
+                data-testid="recipe-category"
+                className="text-md italic lowercase antialiased text-slate-500 mb-5"
+              >
+                {recipeDetails[type[recipeType].category]}
+              </h2>
+              <h3 className="text-xl font-medium mb-2">
+                Ingredients
+              </h3>
+              <ul
+                className="list-disc ml-4 mb-5"
+              >
+                {
+                  recipeDetails?.ingredients
+                    .filter((ingredient) => ingredient !== null && ingredient !== '')
+                    .map((ingredient, index) => (
+                      <li
+                        key={ index }
+                        data-testid={
+                          String(index).concat('-ingredient-name-and-measure')
+                        }
+                      >
+                        <span className="flex gap-2">
+                          <p className="capitalize">{ingredient}</p>
+                          -
+                          <p
+                            className="text-slate-500 italic font-light antialiased"
+                          >
+                            {recipeDetails.measures[index]}
+                          </p>
+                        </span>
+                      </li>
+                    ))
+                }
+              </ul>
+              <h3 className="text-xl font-medium mb-2">
+                Instructions
+              </h3>
+              <span
+                data-testid="instructions"
+              >
+                {recipeDetails.strInstructions.split('.').map((paragraph, i) => (
+                  <p key={ i } className="text-justify leading-relaxed mb-1">
+                    {`${paragraph}${paragraph !== '' ? '.' : ''}`}
+                  </p>
+                ))}
+                {/* {recipeDetails.strInstructions} */}
+              </span>
+            </div>
             {
               recipeType === 'food' && (
-                <iframe
-                  className="mt-10 w-screen h-52"
-                  src={ `https://www.youtube.com/embed/${recipeDetails.strYoutube.split('=')[1]}` }
-                  title={ recipeDetails.strMeal }
-                  frameBorder="0"
-                  allowFullScreen
-                />
+                <>
+                  <h3 className="text-xl font-medium -mb-8 pl-7">
+                    Video
+                  </h3>
+                  <iframe
+                    data-testid="video"
+                    className="mt-10 w-screen h-52"
+                    src={ `https://www.youtube.com/embed/${recipeDetails.strYoutube.split('=')[1]}` }
+                    title={ recipeDetails[type[recipeType].name] }
+                    frameBorder="0"
+                    allowFullScreen
+                  />
+                </>
               )
             }
             <ul>
