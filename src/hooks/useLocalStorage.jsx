a744d1ea5f4bce9548786cdb936321f4b62631db
Raphael Cirelli <raphael.cirelli@protonmail.com>
2022-07-19 22:03:52 -0300

    req 37, 38, e 39 passando nos testes

--- src/hooks/useLocalStorage.jsx
+++ src/hooks/useLocalStorage.jsx
@@ -12,6 +12,7 @@ export default function useLocalStorage(key, initialValue) {
     } else {
       setSavedValue(JSON.parse(savedItem));
     }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
